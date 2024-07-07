1def3db6bc792a9edc677cab21d74c4f64801a28
yuehern <1600084f@student.tp.edu.sg>
2024-07-07 16:10:32 +0800

    Fix auto redirect and allow csv file for upload only

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -103,7 +103,7 @@ const UploadPage = () => {
             setTimeout(() => {
               setIsCreating(false);
               router.push(`/project/${data.id}`);
-            }, 5000);
+            }, 2000);
           })
           .catch((error) => {
             console.error("Error:", error);
